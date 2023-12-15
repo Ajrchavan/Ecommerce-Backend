4d9d9ba711d6ec5913662f3d8cfdfee289e0923f
AjrChavan <ajrrchavan123@gmail.com>
2023-12-15 19:32:08 +0530

    commit message

---
+++ src/main/java/com/spring/repository/OrderRepository.java
@@ -0,0 +1,20 @@
+package com.spring.repository;
+
+import java.util.List;
+import java.lang.*;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.model.PlaceOrder;
+
+@Repository
+@Transactional
+public interface OrderRepository extends JpaRepository<PlaceOrder, Long> {
+
+	PlaceOrder findByOrderId(int orderId);
+
+	List<PlaceOrder> findAll();
+
+}
